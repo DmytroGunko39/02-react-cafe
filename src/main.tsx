d2bb01698dc2e6abcb78723371ef7bbd11969a28
Dmytro_Gunko <dimagunko99@gmail.com>
2025-07-09 17:36:10 +0200

    hw2reactFeedbackApp

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,6 @@
-import "modern-normalize/modern-normalize.css";
 import { createRoot } from "react-dom/client";
+import App from "./components/App/App";
+import "./index.css";
+import "modern-normalize/modern-normalize.css";
 
-createRoot(document.getElementById("root") as HTMLElement).render(
-  <div>
-    <h1>Welcome to React</h1>
-    <p>
-      This is JSX — it looks like HTML, but it's not quite the same. It has its
-      own rules!
-    </p>
-  </div>
-);
+createRoot(document.getElementById("root") as HTMLElement).render(<App />);
